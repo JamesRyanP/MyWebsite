f72533ec4d1ec81d28c820333615a688fd995816
James <james.patzmann@gmail.com>
2023-07-06 17:22:08 -0400

    deploy to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/MyWebsite'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/MyWebsite/index.html'));});
+app.use(express.static('./dist/MyWebsite'));
+
+app.get('/*', (req,res) =>
+    res.sendFile('index.html', {root: 'dist/MyWebsite/'}),
+);
+
+
 app.listen(process.env.PORT || 8080);
